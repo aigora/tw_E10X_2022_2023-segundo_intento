938fa2d6b5fbc841704edcd606563cc94a7b44e4
Arnaiz13 <david.arnaiz@alumnos.upm.es>
2023-05-05 17:23:57 +0200

    Funciones

---
+++ Funcionmax.c
@@ -0,0 +1,12 @@
+float maximo(float vector[11])
+{
+	int i;
+	float max;
+	max=vector[0]
+	for(i=1;i<11;i++)
+	{
+		if(vector[i]>vector[0])
+			max=vecotr[i];
+	}
+	return max;
+}
---
+++ Función_Kwh-euros.c
@@ -0,0 +1,8 @@
+float kwe (float energia)
+{
+	float euros;
+	//factor de conversion de KW/h-euros
+	float factor=0,14207;
+	
+	return euros=factor*energía;
+}
