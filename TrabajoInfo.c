f23867d22fde5516c6bdc7682f80976eaacc4a58
DanielFrenzel <d.frenzel@alumnos.upm.es>
2023-05-11 22:34:56 +0200

    Menú cambiado

--- TrabajoInfo.c
+++ TrabajoInfo.c
@@ -1,12 +1,25 @@
 #include<stdio.h>
 
-float  maximo(float vector[12]);
+float  maximo(float vector[11]);
 int main()
 {
 	int comunidad,i;
 	float hidrau[11],eoli[11],solar[11],termica[11],otras[11],vector[12],max1;
-	char num,num2,num3,texto[30];
+	char renovable,funciones,maximos;
 	FILE *fAndalucia;
+	fAndalucia=fopen("Generacion_Andalucia.csv","r");
+	if(fAndalucia==NULL)
+	{
+		printf("Error al leer el archivo\n");
+		return -1;
+	}
+	else
+	{
+		for(i=0;i<12;i++)
+		{
+			fscanf(fAndalucia,"%f,",&hidrau[i]);
+		}	
+	}
 	do
 	{
 	printf("Elige una comunidad autonoma\n");
@@ -23,175 +36,154 @@ int main()
 					printf("Ahora elige una renovable\n");
 					printf("1.-Hidraulica\n2.-Hidroeolica\n3.-Eolica\n4.-Solar fotovoltaica\n5.-Solar termica\n6.-Otras renovables\n7.-Residuos renovables\n");
 					printf("Pulse A para volver(<----)\n");
-					scanf(" %c",&num);
-					if(num=='a'|| num=='A')
+					scanf(" %c",&renovable);
+					if(renovable=='a')
 					{
 						system("cls");
 						break;
 					}
-					if(num=='1')
+					switch(renovable)
 					{
-						system("cls");
-						printf("Elige la operación que quieres hacer\n");
-						printf("1.-Maximo\n2.-Minimo\n3.-Promedio\n");
-						scanf("   %c",&num2);
-						fAndalucia=fopen("Generacion_Andalucia.csv","r");
-						if(fAndalucia==NULL)
-						{
-							printf("Error al abrir el fichero\n");
-							return -1;
-						}
-						else if(num2=='a'|| num2=='A')
-						{
-							system("cls");
-							comunidad=1;
-						}
-						else
-						{
-							for(i=0;i<12;i++)
+						case '1'://Hidraulica
 							{
-							fscanf(fAndalucia,"%f,",&hidrau[i]);
-							}
-							fclose(fAndalucia);
-							if(num2=='1')
-							{
-								system("cls");
-								max1=maximo(hidrau);
-								printf("El maximo es %f\n",max1);
-								scanf("  %c",&num3);
-								if(num3=='A'|| num3=='A')
+								do
 								{
 									system("cls");
-									num=='1';
-								}
+									printf("Elige la operacion que quieres hacer\n");
+									printf("1.-Maximo\n2.-Minimo\n3.-Promedio\n");
+									printf("Pulse A para volver(<----)\n");
+									scanf("  %c",&funciones);
+									if(funciones=='a')
+									{
+										system("cls");
+										break;
+									}
+									switch(funciones)
+										{											
+											case '1'://Máximo
+											{
+												do
+												{
+													system("cls");
+													max1= maximo(hidrau);
+													printf("El maximo es %f\n",max1);
+													printf("Pulse A para volver(<----)\n");
+													scanf("   %c",&maximos);
+													if(maximos=='a')
+													{
+														system("cls");
+														break;
+													}
+												}while(funciones=='1');
+												break;
+											}
+										}
+								}while(renovable=='1');
+							break;
 							}
-						}	
-					}
-					if(num=='2')
-					{
-						system("cls");
-						printf("suuuuuuuu"); 
-						scanf("  %c",&num);	
-					}
-					if(num=='3')
-					{
-						system("cls");
-						fAndalucia=fopen("Generacion_Andalucia.csv","r");
-						if(fAndalucia==NULL)
-						{
-							printf("Error al abrir el fichero\n");
-							return -1;
-						}
-						else
-						{
-							for(i=0;i<12;i++)
+						case '2'://Hidroeólica
 							{
-							fscanf(fAndalucia,"%f,",&hidrau[i]);
+								do
+								{
+									system("cls");
+									printf("Elige la operacion que quieres hacer\n");
+									printf("1.-Maximo\n2.-Minimo\n3.-Promedio\n");
+									printf("Pulse A para volver(<----)\n");
+									scanf("  %c",&funciones);
+									if(funciones=='a')
+									{
+										system("cls");
+										break;
+									}
+									switch(funciones)
+										{											
+											case '1'://Máximo
+											{
+												do
+												{
+													system("cls");
+													max1= maximo(hidrau);
+													printf("El maximo es %f\n",max1);
+													printf("Pulse A para volver(<----)\n");
+													scanf("   %c",&maximos);
+													if(maximos=='a')
+													{
+														system("cls");
+														break;
+													}
+												}while(funciones=='1');
+												break;
+											}
+										}
+								}while(renovable=='1');
+							break;
 							}
-							
-							fscanf(fAndalucia,"\n");
-							
-							for(i=0;i<12;i++)
-							{
-							fscanf(fAndalucia,"%f,",&eoli[i]);
-							}
-							
-							for(i=0;i<12;i++)
-							{
-								printf("%f\n",eoli[i]);
-							}
-							
-							fclose(fAndalucia);
-								
-							scanf("  %c",&num);
-							if(num=='a'|| num=='A')
-							{
-								system("cls");
-								comunidad=1;
-							}
-						}
 					}
-					if(num=='4')
-					{
-						system("cls");	
-						printf("hola4");
-					}
-					if(num=='5')
-					{
-						system("cls");	
-						printf("hola5");
-					}
-					if(num=='6')
-					{
-						system("cls");	
-						printf("hola6");
-					}
-					if(num=='7')
-					{
-						system("cls");	
-						printf("hola7");
-					}
-					}while(comunidad==1);
-					break;
+				}while(comunidad==1);
+			break;
 			}
 		case 2://Aragón
 			{	
 				do
-				{	
+				{
 					system("cls"); 
 					printf("Ahora elige una renovable\n");
 					printf("1.-Hidraulica\n2.-Hidroeolica\n3.-Eolica\n4.-Solar fotovoltaica\n5.-Solar termica\n6.-Otras renovables\n7.-Residuos renovables\n");
-					printf("Pulse A para volver(<----)");
-					scanf(" %c",&num);
-					if(num=='a'|| num=='A')
+					printf("Pulse A para volver(<----)\n");
+					scanf(" %c",&renovable);
+					if(renovable=='a')
 					{
 						system("cls");
 						break;
 					}
-					if(num=='1')
+					switch(renovable)
 					{
-						system("cls");	
-						printf("hola8\n");
-						
-					}
-					if(num=='2')
-					{
-						system("cls");	
-						printf("hola2");
-					}
-					if(num=='3')
-					{
-						system("cls");	
-						printf("hola3");
-					}
-					if(num=='4')
-					{
-						system("cls");	
-						printf("hola4");
-					}
-					if(num=='5')
-					{
-						system("cls");	
-						printf("hola5");
-					}
-					if(num=='6')
-					{
-						system("cls");	
-						printf("hola6");
-					}
-					if(num=='7')
-					{
-						system("cls");	
-						printf("hola7");
+						case '1'://Hidraulica
+							{
+								do
+								{
+									system("cls");
+									printf("Elige la operacion que quieres hacer\n");
+									printf("1.-Maximo\n2.-Minimo\n3.-Promedio\n");
+									printf("Pulse A para volver(<----)\n");
+									scanf("  %c",&funciones);
+									if(funciones=='a')
+									{
+										system("cls");
+										break;
+									}
+									switch(funciones)
+										{											
+											case '1'://Máximo
+											{
+												do
+												{
+													system("cls");
+													max1= maximo(hidrau);
+													printf("El maximo es %f\n",max1);
+													printf("Pulse A para volver(<----)\n");
+													scanf("   %c",&maximos);
+													if(maximos=='a')
+													{
+														system("cls");
+														break;
+													}
+												}while(funciones=='1');
+												break;
+											}
+										}
+								}while(renovable=='1');
+							break;
+							}
 					}
 				}while(comunidad==2);
 				break;
 			}
 	}
-	}while((comunidad>=1 && comunidad<=19)||(comunidad==0));
+	}while((comunidad>=1 && comunidad<=19));
 	return 0;
 }
-float maximo(float vector[12])
+float maximo(float vector[11])
 {
 	int i;
 	float max;
